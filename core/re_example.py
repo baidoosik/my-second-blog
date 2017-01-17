0fdbe8953d38427e76c15506349b9c9bd46af351
doosik <qoentlr37@naver.com>
2017-01-17 23:42:06 +0900

    1.17 re library search function

---
+++ core/re_example.py
@@ -0,0 +1,19 @@
+import re
+
+# 아래에서 사용된 pattern은
+# (\w+[\w\.]*) 2개와 ([A-Za-z]+) 총 세 개의 하위 표현식으로 작성되어 있다.
+m = re.search(r"(\w+[\w\.]*)@(\w+[\w\.]*)\.([A-Za-z]+)",
+              "My email address is SooKkaRak@gmail.com")
+print('m.group():')
+print(m.group())
+print('m.groups():')
+print(m.groups())
+
+result = m.groups()
+name =['m.group[0]','m.group[1]','m.group[2]']
+
+num=[0,1,2]
+
+for i in num:
+    print(name[i])
+    print(result[i])
