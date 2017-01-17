d3c895220205c20c952f837d233d7e6161097b81
doosik <qoentlr37@naver.com>
2017-01-17 23:45:32 +0900

    1.17 re library search function2

--- core/re_example.py
+++ core/re_example.py
@@ -1,4 +1,6 @@
 import re
+#re library참고 http://egloos.zum.com/sweeper/v/3065126
+
 
 # 아래에서 사용된 pattern은
 # (\w+[\w\.]*) 2개와 ([A-Za-z]+) 총 세 개의 하위 표현식으로 작성되어 있다.
@@ -17,3 +19,4 @@ num=[0,1,2]
 for i in num:
     print(name[i])
     print(result[i])
+
