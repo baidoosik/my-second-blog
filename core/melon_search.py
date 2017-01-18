67b0305c298c3c9bf0dda0d175b62d208d7b1601
doosik <qoentlr37@naver.com>
2017-01-19 01:31:51 +0900

    1.19 멜론서치코드

--- core/melon_search.py
+++ core/melon_search.py
@@ -1,5 +1,29 @@
 import requests as req
+import json
 
-url ="http://www.melon.com/search/keyword/index.json?jscallback=jQuery191021634718864107283_1484665486657&query=%25E3%2585%258C&_=1484665486673"
+#mac alfred2 lancher tool
+#https://github.com/askdjango/snippets
 
-print(req.get(url).text)
+def melon_search(q):
+    url ="http://www.melon.com/search/keyword/index.json?"
+
+    param = {
+        'jscallback':'jQuery191021634718864107283_1484665486657',
+        'query':q,
+    }
+    html = req.get(url,params=param).text
+
+    html= html.replace(param['jscallback']+'(','').replace(');','')
+    response_dict = json.loads(html)
+
+    if 'SONGCONTENTS' not in response_dict:
+        print('result is not found')
+    else:
+        for song in response_dict['SONGCONTENTS']:
+            print('''{SONGNAME} {SONGID} {ARTISTNAME}
+            - http://www.melon.com/album/detail.htm?albumId={ALBUMID}'''.format(**song))
+            #print(song['SONGNAME'],song['SONGID'],song['ARTISTNAME'])
+
+if __name__ == '__main__' :
+    line =input()
+    print(melon_search(line))
