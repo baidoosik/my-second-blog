fef4d01fd75d106b5b7e87fbded9aa99517c71c6
doosik <qoentlr37@naver.com>
2017-01-21 00:04:59 +0900

    1.21 국회의원 명단 크롤링 마무리

--- core/assembly_member.py
+++ core/assembly_member.py
@@ -1,17 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
-url = "http://www.assembly.go.kr/assm/memact/congressman/memCond/memCondListAjax.do"
-params = {
-    'currentPage': 1,
-    'rowPerPage': 300
-}
+url = "http://www.assembly.go.kr/assm/memact/congressman/memCond/memCondListAjax.do?currentPage=1&rowPerPage=300"
 
-html = requests.get(url,params=params).text
+html = requests.get(url).text
 
 soup = BeautifulSoup(html, 'html.parser')
 
-for tag in soup.select('.memberna_list dl dt a'):
-    name = tag.text
-    id = tag['href']
-    print(name, id)
+for tag_member in soup.select('.memberna_list dl dt a'):
+    name = tag_member.text
+    link = tag_member['href'].replace('javascript:jsMemPop(','').replace(')','')
+    print(name,link)
+
